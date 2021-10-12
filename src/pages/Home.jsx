5d6ed3cdcfa2c1bbfa32bd26efcd093707d16983
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-12 19:07:16 -0300

    adding projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,11 +19,18 @@ const Home = () => {
         world. Throughout the years I created Android Apps with Java, models of
         Deep Learning with Python, AI with Javascript, but currently I'm focused
         on Cryptos using technologies like Web3, Ethers, Truffle, Solidity,
-        React, NextJS and so on. I'm now building DeFi and NFTs DApps. I also
-        create Backend servers with Node, made some bots with it.
+        React, NextJS and so on. I'm now building DeFi and NFT DApps. I also
+        build Backend servers with Node, made some bots with it.
         <br></br>
         <br></br>
-        &nbsp;
+        &nbsp; I speak English, French and Portuguese
+        <small style={{ fontSize: "15px", fontStyle: "italic" }}>
+          {" "}
+          [currently learning Russian]
+        </small>
+        . I like learning new things related to Science or new Technologies and
+        on my free time I like to watch code magicians like Daniel Shiffman, CJ
+        from The Coding Garden and George Hotz.
       </div>
       <br></br>
       <strong>in construction...</strong>
