5dc1b565fda80178f922041c43154e8b98bcd6c8
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-22 13:14:00 -0300

    updating

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -24,10 +24,10 @@ const projects = [
   {
     title: "ERC721 Holder built from scratch",
     description:
-      "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. A ERC721 Holder you can mint 'random' assets of different colors, in this case the user can Mint tokens for free, just paying the gas to call the function. the website runs perfectly but the program I use to capture the gifs (Giphy) lets the internet slow. I believe CryptoKitties where made similar to this with some mutation instead of random colors, which maybe I will try to replicate for learning purposes.",
+      "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. A ERC721 Holder you can mint 'random' assets of different colors, in this case the user can Mint tokens for free, just paying the gas to call the function. The website runs perfectly but the program I use to capture the gifs (Giphy) slows down the internet. I believe CryptoKitties where made similar to this with some mutation instead of random colors, which maybe I will try to replicate for learning purposes.",
     image: (
       <img
-        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/erc721.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/erc721.gifg"
         width="100%"
         alt=""
       ></img>
@@ -37,7 +37,7 @@ const projects = [
   {
     title: "Decentralized Video Uploader",
     description:
-      "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. It's just like YouTube where you can upload videos and get some tips in Ether and NerdCoin, a ERC20 token I created. The videos are being uploaded to IPFS.((the website runs perfectly but the program I use to capture the gifs (Giphy) lets the internet slow.))",
+      "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. It's just like YouTube where you can upload videos and get some tips in Ether and NerdCoin, a ERC20 token I created. The videos are being uploaded to IPFS. The website runs perfectly but the program I use to capture the gifs (Giphy) slows down the internet.",
     image: (
       <img
         src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif18.gif"
