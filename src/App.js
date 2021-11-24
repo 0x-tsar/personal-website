b14407073e4abed153821dddd4066fc92990a5c1
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-24 20:40:23 -0300

    :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <title>Gustavo Fernandes</title>
+    </div>
+  );
 }
 
 export default App;
