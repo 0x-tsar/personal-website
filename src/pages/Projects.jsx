e5a35adbd8156e411e16d43f02c415d5a58b8e50
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-28 05:43:57 -0300

    :sparkles:

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -299,6 +299,7 @@ const Projects = () => {
           <Post key={key}>
             <h4>{item.title}</h4>
             <p>{item.description}</p>
+            <br />
             <a>{item.website && item.website}</a>
             <div style={{ display: "flex", justifyContent: "center" }}>
               {item.image}
