b7081a2e4502363bac82a01e61b9a9f2500a1233
Gustavo <contato.gustavofernandes@hotmail.com>
2022-01-29 06:48:53 -0300

    setting back to google storage

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,7 +18,7 @@ export const Post = styled.div`
   margin-bottom: 20px;
 `;
 
-<img src="erc721.gif" width="1500px" height="700px"></img>;
+{/* <img src="erc721.gif" width="1500px" height="700px"></img>; */}
 
 const projects = [
   {
@@ -28,8 +28,8 @@ const projects = [
     website: "https://tiktok-next.herokuapp.com/",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ss1.png"
-        src="./images/ss1.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ss1.png"
+        // src="./images/ss1.png"
         width="100%"
         alt=""
       ></img>
@@ -43,8 +43,8 @@ const projects = [
     website: "https://lottery-chainlink.herokuapp.com/",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/dice.png"
-        src="./images/chainlink.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/dice.png"
+        // src="./images/chainlink.png"
         width="100%"
         alt=""
       ></img>
@@ -56,8 +56,8 @@ const projects = [
       "Interacting with IPFS through Fleek and using the blockchain to store information about the book's details. Soon will be adding project repository",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/books.png"
-        src="./images/books.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/books.png"
+        // src="./images/books.png"
         width="100%"
         alt=""
       ></img>
@@ -69,8 +69,8 @@ const projects = [
       "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. A ERC721 Holder you can mint 'random' assets of different colors, in this case the user can Mint tokens for free, just paying the gas to call the function. The website runs perfectly but the program I use to capture the gifs (Giphy) slows down the internet. I believe CryptoKitties were made similar to this with some mutation instead of random colors, which maybe I will try to replicate for learning purposes.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/erc721.gif"
-        src="./images/erc721.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/erc721.gif"
+        // src="./images/erc721.png"
         width="100%"
         alt=""
       ></img>
@@ -83,8 +83,8 @@ const projects = [
       "In this project I used ReactJS, Truffle, Solidity, NodeJS and Ganache to support the network. It's just like YouTube where you can upload videos and get some tips in Ether and NerdCoin, a ERC20 token I created. The videos are being uploaded to IPFS. The website runs perfectly but the program I use to capture the gifs (Giphy) slows down the internet.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif18.gif"
-        src="./images/gif18.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif18.gif"
+        // src="./images/gif18.gif"
         width="100%"
         alt=""
       ></img>
@@ -97,8 +97,8 @@ const projects = [
       "One of my first DApps built in 2017 with Solidity, HTML, CSS and JQuery.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif5.gif"
-        src="./images/ggif5.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif5.gif"
+        // src="./images/ggif5.gif"
         width="100%"
         alt=""
       ></img>
@@ -111,8 +111,8 @@ const projects = [
       "A clone of a known sports website, I used NextJS and styled-components for structuring, still in progress.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ss2.png"
-        src="./images/ss2.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ss2.png"
+        // src="./images/ss2.png"
         width="100%"
         alt=""
       ></img>
@@ -126,8 +126,8 @@ const projects = [
       "Model built on Python, no Data Augmentation, few samples, not tested on training data despite similar look, they are slightly rotated. (No MaxPooling2D nor UpSampling2D were used, just CNNS and Dense Layers)",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/colorization.png"
-        src="./images/colorization.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/colorization.png"
+        // src="./images/colorization.png"
         width="100%"
         alt=""
       ></img>
@@ -138,8 +138,8 @@ const projects = [
     description: "Model built on Python and samples picked by Android device.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif8.gif"
-        src="./images/gif8.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif8.gif"
+        // src="./images/gif8.gif"
         width="100%"
       ></img>
     ),
@@ -150,8 +150,8 @@ const projects = [
     description: "Smart Contract created with Solidity, HTML, CSS, Javascript.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif9.gif"
-        src=".images//gif9.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif9.gif"
+        // src=".images//gif9.gif"
         width="100%"
       ></img>
     ),
@@ -163,9 +163,9 @@ const projects = [
       "Model built on Python and Android app with Java, it sends a notification when the door is opened or closed to device. The door is checked by a webcam (On video the sound can be heard saying 'The Door was opened' and 'The door was closed').",
     image: (
       // <img
-      // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif10.gif"
+      src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif10.gif"
       // ></img>
-      <img src="./images/gif10.gif"></img>
+      // <img src="./images/gif10.gif"></img>
     ),
   },
 
@@ -175,8 +175,8 @@ const projects = [
       "Built on Python and Keras (9 Epochs, 1 hour each, 9.861 samples, no Data Augmentation, MaxPooling2D and UpSampling2D was used on this case), A lot of space for improvement.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/smp.png"
-        src="./images/smp.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/smp.png"
+        // src="./images/smp.png"
         width="100%"
         height="730px"
       ></img>
@@ -189,8 +189,8 @@ const projects = [
       "I'm building a Tinder Bot, but first I need samples to classify my preferences, so I'm WebScrapping Tinder to get each picture and train them later, all the options are being rejected so it doesn't hit the day limit for a free account, so I have access to all the images avaiable, then after training the neural network with more than 30.000 samples, I'm going to delete this account to zero out the score and start again with a trained network with a premium account, having also unlimited picks sliding to right, increasing the database for further use and improvement.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif11.gif"
-        src="./images/gif11.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif11.gif"
+        // src="./images/gif11.gif"
         width="100%"
       ></img>
     ),
@@ -202,8 +202,8 @@ const projects = [
       "This is a proof-of-concept of a Neural Network finding the object's coordinates with just 900 labeled training dataset and 100 for test (generated randomly). Can EASILY be improved just by increasing the number of samples, the ones displaying that are not so accurate were purposely picked to show the `worst cases` because the majority of the samples were perfect, the most complex part was cleaning and preparing the data to get just the necessary numbers. All of these squares were generated after the training, none of these are in the training set, so it behaved pretty well by the small amount of samples. This type of concept can have multiple types of utilities because not only it can classify the object but also tell you where it is. ",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/finding.png"
-        src="./images/finding.png"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/finding.png"
+        // src="./images/finding.png"
         width="100%"
       ></img>
     ),
@@ -216,8 +216,8 @@ const projects = [
 
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif12.gif"
-        src="./images/gif12.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif12.gif"
+        // src="./images/gif12.gif"
         height="1000px"
       ></img>
     ),
@@ -228,8 +228,8 @@ const projects = [
       "I cloned an Instagram based decentralized project and created my own ERC-20 coin and added to it so I could also tip the photos with this new coin. Also made some changes with ReactJS",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif17.gif"
-        src="./images/gif17.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif17.gif"
+        // src="./images/gif17.gif"
         width="100%"
         height="700px"
       ></img>
@@ -241,9 +241,9 @@ const projects = [
       "This is a better Tinder bot buit from scratch with new features, now it's able to change cities when no one is found at the city you are located at, the bot changes the city according to the cities I want to go to and starts the process normally again, I also solved a loop problem, after a certain loop time, the bot would break because of 'Inifite Loop', so I created a small loop to go for 600 profiles then break the small loop and count +1 on the bigger one, like a small gear moving a bigger one, worked perfectly! in 2 weeks it got more than 1100 matches and goes on non-stop. This bot made more than 4000 matches before I turned it off.",
     image: (
       // <img
-      // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif15.gif"
+      src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif15.gif"
       // ></img>
-      <img src="./images/gif15.gif"></img>
+      // <img src="./images/gif15.gif"></img>
     ),
   },
   {
@@ -251,15 +251,15 @@ const projects = [
     description:
       "Built with Javascript and TensorFlow and trained on web client-side. (Just CNN and Dense layers used).",
     image: (
-      // <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif5.gif"></img>
-      <img src="./images/gif5.gif"></img>
+      <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif5.gif"></img>
+      // <img src="./images/gif5.gif"></img>
     ),
   },
   {
     title: "Sequence of bottle caps",
     description:
       "Built with Javascript and TensorFlow and trained on web client-side. (No type of RNN were used here).",
-    image: <img src="./images/gif6.gif" width="100%"></img>,
+    // image: <img src="./images/gif6.gif" width="100%"></img>,
   },
 
   {
@@ -267,8 +267,8 @@ const projects = [
     description:
       "Device Generates random colors and depending on the color picked it sends to Firebase database the data and predicts on Neural Network client side on web (HTML,CSS,JS,TensorFlow) and sends result predicted back to device.",
     image: (
-      // <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif3.gif"></img>
-      <img src="./images/ggif3.gif"></img>
+      <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif3.gif"></img>
+      // <img src="./images/ggif3.gif"></img>
     ),
   },
 
@@ -276,7 +276,7 @@ const projects = [
   //   title: "Sample Picker",
   //   description:
   //     "Simple sample picker to train later built on Java, data already classified.",
-  //   image: <img src="ggif2.gif" width="100%"></img>,
+    image: <img src="ggif2.gif" width="100%"></img>,
   // },
 
   {
@@ -284,8 +284,8 @@ const projects = [
     description: "Store built using Java and Firebase. (not finished)",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif1.gif"
-        src="./images/ggif1.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/ggif1.gif"
+        // src="./images/ggif1.gif"
         width="100%"
       ></img>
     ),
@@ -296,8 +296,8 @@ const projects = [
       "Built on Android, testing knowledge about important figures of History.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif7.gif"
-        src="./images/gif7.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif7.gif"
+        // src="./images/gif7.gif"
         width="100%"
       ></img>
     ),
@@ -308,8 +308,8 @@ const projects = [
     description:
       "Simple chat for Android made with java with functionalities like last seen, message visualized and also emojis avaiable.",
     image: (
-      // <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif13.gif"></img>
-      <img src="./images/gif13.gif"></img>
+      <img src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif13.gif"></img>
+      // <img src="./images/gif13.gif"></img>
     ),
   },
 
@@ -319,8 +319,8 @@ const projects = [
       "Built in Javascript using Tensorflow with Convolutional, MaxPooling2D and Dense layers. Samples had 40x40 in size with 1 gray channel, I removed the other ones because it was too heavy, actually it worked REALLY well as we can see, despite the ilumination. 554 samples were picked, roughly 200 for each book and the rest for NO BOOK and ran through 25 epochs (not even necessary that much). I implemented a section on the left side to give the links and price to the books recognized. Also all the computation was made client side, no server was necessary here.",
     image: (
       <img
-        // src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif14.gif"
-        src="./images/gif14.gif"
+        src="https://storage.cloud.google.com/staging.recycler-teste.appspot.com/gif14.gif"
+        // src="./images/gif14.gif"
         width="100%"
       ></img>
     ),
@@ -356,7 +356,11 @@ const Projects = () => {
               <div></div>
             )}
 
-            {/* <img src="./logo512.png" alt="" width={100} /> */}
+            {
+              {
+                // /* <img src="./logo512.png" alt="" width={100} /> */
+              }
+            }
           </Post>
         );
       })}
